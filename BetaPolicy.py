503afca7e5f1750de10777ac5e6db6901d68c471
robert1003 <b07902047@ntu.edu.tw>
2022-10-01 15:45:06 -0400

    update BetaPolicy

--- BetaPolicy.py
+++ BetaPolicy.py
@@ -1,9 +1,9 @@
 import torch
 
 class BetaPolicy(torch.nn.Module):
-	def __init__(self):
+	def __init__(self, input_dim):
 		super(BetaPolicy,self).__init__()
-		self.linear = torch.nn.Linear(4,1)
+		self.linear = torch.nn.Linear(input_dim, 1)
 
 	def forward(self,x):
 		output = torch.sigmoid(self.linear(x))
