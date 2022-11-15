4bce0b69a18c17d474841f2b998426226866b082
robert1003 <robert881003@gmail.com>
2022-11-15 18:08:46 -0500

    make agent pair ordered

--- Environment.py
+++ Environment.py
@@ -61,7 +61,9 @@ class Environment:
         for node, agent_list in pos_agent_list.items():
             permu = self.rng.permutation(agent_list)
             for i in range(0, len(permu)-1, 2):
-                agent_pairs.append((permu[i], permu[i + 1]))
+                mn = min(permu[i], permu[i + 1])
+                mx = max(permu[i], permu[i + 1])
+                agent_pairs.append((mn, mx))
 
         logging.debug('env step done with paired agent {}'.format(agent_pairs))
 
