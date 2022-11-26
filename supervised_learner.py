e0cf3b140ab651961a98a57627ba1f61f84b2690
Suhas <suhasraja1@gmail.com>
2022-11-26 12:26:12 -0600

    train and dump sl/lr model

--- supervised_learner.py
+++ supervised_learner.py
@@ -1,12 +1,24 @@
 import pandas
 from sklearn import model_selection
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LinearRegression
 import pickle as pk
 dataframe = pandas.read_csv("data.csv")
 array = dataframe.values
-X = array[:,0:4]
-Y = array[:,4]
-model = LogisticRegression()
+X = array[:,1:5]
+Y = array[:,5]
+Y = np.clip(Y, 1e-8, 1 - 1e-8)   # numerical stability
+Y = np.log(Y / (1 - Y))  # transform to log-odds-ratio space
+model = LinearRegression()
 model.fit(X, Y)
 
-pk.save("lr_beta.pkl")
+class SLBetaModel:
+	def __init__(self, model):
+		self.model = model
+	def predict(val):
+		return self.sigmoid(self.model.predict(val))
+	def sigmoid(x):
+	    ex = np.exp(x)
+	    return ex / (1 + ex)
+
+with open("sl_beta.pkl",'wb') as fp:
+	pk.dump(SLBetaModel(model),fp)
