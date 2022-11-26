76ef1b3466c8e24a3c291d3b4004f6bce8c98e81
robert1003 <b07902047@ntu.edu.tw>
2022-11-26 15:26:19 -0500

    update supervised beta learner training script

--- supervised_learner.py
+++ supervised_learner.py
@@ -1,15 +1,8 @@
-import pandas
-from sklearn import model_selection
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-import pickle as pk
-dataframe = pandas.read_csv("data.csv")
-array = dataframe.values
-X = array[:,1:5]
-Y = array[:,5]
-Y = np.clip(Y, 1e-8, 1 - 1e-8)   # numerical stability
-Y = np.log(Y / (1 - Y))  # transform to log-odds-ratio space
-model = LinearRegression()
-model.fit(X, Y)
+from sklearn.metrics import mean_squared_error, mean_absolute_error
 
 class SLBetaModel:
 	def __init__(self, model):
@@ -20,5 +13,37 @@ class SLBetaModel:
 	    ex = np.exp(x)
 	    return ex / (1 + ex)
 
-with open("sl_beta.pkl",'wb') as fp:
+df = pd.read_csv('data.csv')
+X, y = df.iloc[:, 1:5].to_numpy(), df.iloc[:, 5].to_numpy()
+y = np.clip(y, 1e-8, 1-1e-8)
+train_X, test_X, train_y, test_y = train_test_split(X, y, train_size=0.75)
+
+# normal LR
+print('Linear Regression')
+model = LinearRegression().fit(train_X, train_y)
+pred_y = model.predict(train_X)
+pred_y = np.clip(pred_y, 0, 1)
+print('train mse', mean_squared_error(train_y, pred_y))
+print('train mae', mean_absolute_error(train_y, pred_y))
+pred_y = model.predict(test_X)
+pred_y = np.clip(pred_y, 0, 1)
+print('test mse', mean_squared_error(test_y, pred_y))
+print('test mae', mean_absolute_error(test_y, pred_y))
+
+with open("sl_beta_linear_reg.pkl",'wb') as fp:
+	pk.dump(SLBetaModel(model),fp)
+
+# log-odds-ratio
+print('log-odd-ratio')
+model = LinearRegression().fit(train_X, np.log(train_y / (1 - train_y)))
+pred_y = model.predict(train_X)
+pred_y = np.exp(pred_y) / (1 + np.exp(pred_y))
+print('train mse', mean_squared_error(train_y, pred_y))
+print('train mae', mean_absolute_error(train_y, pred_y))
+pred_y = model.predict(test_X)
+pred_y = np.exp(pred_y) / (1 + np.exp(pred_y))
+print('test mse', mean_squared_error(test_y, pred_y))
+print('test mae', mean_absolute_error(test_y, pred_y))
+
+with open("sl_beta_log_odd_ratio_linear_reg.pkl",'wb') as fp:
 	pk.dump(SLBetaModel(model),fp)
